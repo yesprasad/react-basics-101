81f593857f9acfeb00529333bf41cd50d7203982
Eshwar Yaddanapudi <y.eshwarprasad@gmail.com>
2018-04-30 23:46:33 -0700

    refactored header.js for modularasation

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,17 +2,30 @@ import React from 'react';
 import './header.css';
 
 const Header =() => {
-    return(
-        <header className='header'>
-         <div class="logo-box">
-        <img className='logo' src="images/ipl_logo.png" alt='ipl logo'/>
-        </div>
-            <div className='text-box'>
+
+    const logo = () => {
+        return(
+            <div class="logo-box">
+                <img className='logo' src="images/ipl_logo.png" alt='ipl logo'/>
+            </div>
+        );
+    };
+
+    const headerText =() => {
+       return(
+        <div className='text-box'>
             <h2 className='heading-primary'>
             <span className='heading-primary-main'>DON'T JUST WATCH</span>
             <span className='heading-primary-sub'>Play the IPL</span>
             </h2>
-            </div>
+        </div>
+       );
+    };
+
+    return(
+        <header className='header'>
+         {logo()}
+         {headerText()}
         </header>
     )
 }
